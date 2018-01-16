647057b2114ae5daa5e6b8c51633c44bd6deddd1
Henning baldersheim <balder@yahoo-inc.com>
2018-01-16 21:22:02 +0000

    Implement the jni stuff too.

--- searchcore/src/apps/verify_ranksetup/com_yahoo_vespa_model_application_validation_NativeVerifyRankSetup.cpp
+++ searchcore/src/apps/verify_ranksetup/com_yahoo_vespa_model_application_validation_NativeVerifyRankSetup.cpp
@@ -1,10 +1,20 @@
 #include "com_yahoo_vespa_model_application_validation_NativeVerifyRankSetup.h"
 #include "verify_ranksetup.h"
+#include <stdexcept>
 
 JNIEXPORT jboolean
-JNICALL Java_com_yahoo_vespa_model_application_validation_NativeVerifyRankSetup_verify(JNIEnv *, jclass, jstring)
+JNICALL Java_com_yahoo_vespa_model_application_validation_NativeVerifyRankSetup_verify(JNIEnv * env, jclass, jstring jconfigId)
 {
-    const char * configId = "";
-    bool ok = verifyRankSetup(configId);
+    const char *configId = env->GetStringUTFChars(jconfigId, NULL);
+    if (NULL == configId) return false;
+
+    bool ok = false;
+    try {
+        ok = verifyRankSetup(configId);
+    } catch (std::exception &) {
+
+    }
+    env->ReleaseStringUTFChars(jconfigId, configId);
+    return static_cast<jboolean>(ok);
 }
 
